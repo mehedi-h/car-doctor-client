e0d861a01fd9892a8458fe39219acc96d0212c59
mehedi-h <mehedihs2015@gmail.com>
2023-10-28 14:12:33 +0600

    create a basic stacture and react route

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+
+
+const Home = () => {
+    return (
+        <div>
+            <h1 className="text-4xl font-extrabold text-gray-700">This is Home</h1>
+        </div>
+    );
+};
+
+export default Home;
