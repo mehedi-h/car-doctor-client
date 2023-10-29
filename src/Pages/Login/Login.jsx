822affd8d46e57acb010125bd7ad2fde60e9df99
mehedi-h <mehedihs2015@gmail.com>
2023-10-29 20:03:02 +0600

    created a sign up page

---
+++ src/Pages/Login/Login.jsx
@@ -0,0 +1,70 @@
+import { Link } from "react-router-dom";
+import img from "../../assets/images/login/login.svg";
+
+const Login = () => {
+  const handleLogin = (e) => {
+    e.preventDefault();
+    const form = e.target;
+  };
+
+  return (
+    <div className="hero min-h-screen">
+      <div className="hero-content flex-col lg:flex-row">
+        <div className="md:mr-14 w-1/2">
+          <img src={img} alt="" />
+        </div>
+
+        <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+          <div className="card-body">
+            <h1 className="text-xl md:text-3xl font-bold text-center">Login</h1>
+            <form onSubmit={handleLogin}>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="email"
+                  placeholder="email"
+                  name="email"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  type="password"
+                  placeholder="password"
+                  name="password"
+                  className="input input-bordered"
+                  required
+                />
+                <label className="label">
+                  <a href="#" className="label-text-alt link link-hover">
+                    Forgot password?
+                  </a>
+                </label>
+              </div>
+              <div className="form-control mt-6">
+                <input
+                  type="submit"
+                  value="Login"
+                  className="btn btn-primary"
+                />
+              </div>
+            </form>
+            <p className="my-4 text-center">
+              New to car doctor, Please <Link 
+              className="text-red-500 md:font-semibold"
+              to="/signup">Sign Up</Link>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
---
+++ src/Pages/Signup/Signup.jsx
@@ -0,0 +1,70 @@
+import { Link } from "react-router-dom";
+import img from "../../assets/images/login/login.svg";
+
+const SignUp = () => {
+
+    const handleSignup = e => {
+        e.preventDefault();
+    }
+    
+    return (
+        <div className="hero min-h-screen">
+      <div className="hero-content flex-col lg:flex-row">
+        <div className="md:mr-14 w-1/2">
+          <img src={img} alt="" />
+        </div>
+
+        <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+          <div className="card-body">
+            <h1 className="text-xl md:text-3xl font-bold text-center">Login</h1>
+            <form onSubmit={handleSignup}>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="email"
+                  placeholder="email"
+                  name="email"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  type="password"
+                  placeholder="password"
+                  name="password"
+                  className="input input-bordered"
+                  required
+                />
+                <label className="label">
+                  <a href="#" className="label-text-alt link link-hover">
+                    Forgot password?
+                  </a>
+                </label>
+              </div>
+              <div className="form-control mt-6">
+                <input
+                  type="submit"
+                  value="Login"
+                  className="btn btn-primary"
+                />
+              </div>
+            </form>
+            <p className="my-4 text-center">
+              Already have an Account, Please <Link 
+              className="text-red-500 md:font-semibold"
+              to="/login">Log in</Link>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    );
+};
+
+export default SignUp;
